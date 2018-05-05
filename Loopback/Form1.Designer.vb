5a860518bdd2d3d865fb5f69056e3502aaa04409
Dispnt <yuyuanjiajian@gmail.com>
2018-05-05 18:39:51 +0800

    first commit

--- Loopback/Form1.Designer.vb
+++ Loopback/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
-    Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
 
     'Form 重写 Dispose，以清理组件列表。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,81 @@ Partial Class Form1
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Enable = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialDivider1 = New MaterialSkin.Controls.MaterialDivider()
+        Me.Disable = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(12, 77)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(444, 240)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Enable
+        '
+        Me.Enable.Depth = 0
+        Me.Enable.Location = New System.Drawing.Point(12, 336)
+        Me.Enable.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Enable.Name = "Enable"
+        Me.Enable.Primary = True
+        Me.Enable.Size = New System.Drawing.Size(179, 30)
+        Me.Enable.TabIndex = 4
+        Me.Enable.Text = "Enable"
+        Me.Enable.UseVisualStyleBackColor = True
+        '
+        'MaterialDivider1
+        '
+        Me.MaterialDivider1.BackColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialDivider1.Depth = 0
+        Me.MaterialDivider1.Location = New System.Drawing.Point(0, 329)
+        Me.MaterialDivider1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialDivider1.Name = "MaterialDivider1"
+        Me.MaterialDivider1.Size = New System.Drawing.Size(492, 1)
+        Me.MaterialDivider1.TabIndex = 7
+        Me.MaterialDivider1.Text = "MaterialDivider1"
+        '
+        'Disable
+        '
+        Me.Disable.AutoSize = True
+        Me.Disable.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Disable.BackColor = System.Drawing.Color.White
+        Me.Disable.Depth = 0
+        Me.Disable.ForeColor = System.Drawing.Color.White
+        Me.Disable.Location = New System.Drawing.Point(390, 334)
+        Me.Disable.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.Disable.MouseState = MaterialSkin.MouseState.HOVER
+        Me.Disable.Name = "Disable"
+        Me.Disable.Primary = True
+        Me.Disable.Size = New System.Drawing.Size(66, 36)
+        Me.Disable.TabIndex = 8
+        Me.Disable.Text = "Disable"
+        Me.Disable.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(468, 376)
+        Me.Controls.Add(Me.Disable)
+        Me.Controls.Add(Me.MaterialDivider1)
+        Me.Controls.Add(Me.Enable)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Status:Ready"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Enable As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialDivider1 As MaterialSkin.Controls.MaterialDivider
+    Friend WithEvents Disable As MaterialSkin.Controls.MaterialFlatButton
 End Class
--- Loopback/Form1.vb
+++ Loopback/Form1.vb
@@ -1,3 +1,73 @@
-﻿Public Class Form1
+﻿Imports MaterialSkin
+Imports System.IO
+Imports System.Environment
+Public Class Form1
+    '========================MaterialSkin部分
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+        SkinManager.AddFormToManage(Me)
+        SkinManager.Theme = MaterialSkinManager.Themes.LIGHT
+        SkinManager.ColorScheme = New ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE)
+    End Sub
+
+    '===============获得输出所用类
+    Public Class ConsoleOutput
+        Private Sub New()
+        End Sub
+
+        Private Shared gWorkingDirectory As String = Environment.GetFolderPath(Environment.SpecialFolder.Personal)
+
+        Public Shared Property WorkingDirectory() As String
+            Get
+                Return gWorkingDirectory
+            End Get
+            Set(ByVal Value As String)
+                gWorkingDirectory = Value
+            End Set
+        End Property
+
+        Public Shared Function ExcuteCmd(ByVal command As String) As String
+            Dim mResult As String
+            Dim tmpProcess As New Process
+            With tmpProcess
+                With .StartInfo
+                    .CreateNoWindow = True
+                    .FileName = .EnvironmentVariables.Item("ComSpec")
+                    .RedirectStandardOutput = True
+                    .UseShellExecute = False
+                    .Arguments = String.Format("/C {0}", command)
+                    .WorkingDirectory = gWorkingDirectory
+                End With
+                Try
+                    .Start()
+                    .WaitForExit(5000)
+                    mResult = .StandardOutput.ReadToEnd
+                Catch e As System.ComponentModel.Win32Exception
+                    mResult = e.ToString
+                End Try
+            End With
+            Return mResult
+        End Function
+    End Class
+
+
+    Private Sub Enable_Click_1(sender As Object, e As EventArgs) Handles Enable.Click
+        Me.Text = "Status:Applying"
+        Me.Refresh()
+        Dim UserPath As String = GetFolderPath(SpecialFolder.UserProfile)
+        Dim UWPPath As New DirectoryInfo(UserPath & "\AppData\Local\Packages")
+        ListBox1.Items.Clear()
+        For Each UWPmane In UWPPath.GetDirectories
+            ListBox1.Items.Add(ConsoleOutput.ExcuteCmd("CheckNetIsolation LoopbackExempt -a -n=" + UWPmane.Name) + UWPmane.Name)
+        Next
+        Me.Text = "Status:Done"
+        Me.Refresh()
+    End Sub
+
+    Private Sub Disbale_Click(sender As Object, e As EventArgs) Handles Disable.Click
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add(ConsoleOutput.ExcuteCmd("CheckNetIsolation LoopbackExempt -c"))
+    End Sub
+
 
 End Class
